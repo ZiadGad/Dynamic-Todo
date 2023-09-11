5b2685b4270b4c1a0ac1f0d77ca95173379b7474
ZiadGad <ziadamrjad@gmail.com>
2023-09-11 15:31:34 +0300

    Adding index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
-mongoose.connect("mongodb+srv://ziadgad:161123Gaad@cluster0.lrktwao.mongodb.net/todolistDB");
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
 const itemsSchema = new mongoose.Schema({
         name:{
             type:String,
